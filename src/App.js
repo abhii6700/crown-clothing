f51e56feb851329e257235927d0f696f901f55f4
abhii6700 <abhishekyadav6700.ay@gmail.com>
2021-04-23 23:08:24 +0530

    added form input component and custom button component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component'
 import ShopPage from '../src/pages/shop/shop.component'
 import Header from './component/header/header.component'
+import SignUpAndSignInPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignUpAndSignInPage}/>
       </Switch>
     </div>
   );
